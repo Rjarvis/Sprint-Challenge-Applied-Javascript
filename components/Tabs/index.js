e93d37f7a0ab2150fa5c24f3093c2198618d5e65
Roland J Jarvis <Rjarvis87@gmail.com>
2019-07-20 13:17:01 -0400

    Tabs Component working

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,23 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+function Tab(data){
+    const div = document.createElement('div');
+    div.classList.add('tab');
+    div.textContent = data;
+    return div;
+}
+
+const getData = axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((axiosData) =>{
+        console.log("data", axiosData);
+        const topics = axiosData.data.topics;
+        const parent = document.querySelector('.topics');
+        topics.forEach(element => {
+            var tab = Tab(element);
+            console.log(parent);
+            parent.appendChild(tab);
+        });
+    })
