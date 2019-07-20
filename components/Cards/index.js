648192f9aca81c7091e87870fa134eee6460ce02
Roland J Jarvis <Rjarvis87@gmail.com>
2019-07-20 13:52:06 -0400

    axios data can get

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,36 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function Article(data){
+    function createElement(element, style, content){
+        const div = document.createElement(element);
+        if(style != null){ div.classList = style; }
+        if(content != null){ div.textContent = content; }
+        
+        return div;
+    }
+    const div = createElement('div', "card");
+    const headline = createElement('div','headline');
+    const author = createElement('div', 'author');
+    const authorImg = createElement('div', 'img-container');
+    const img = createElement('img');
+    // img.src = data;
+    authorImg.appendChild(img);
+    const authorName = createElement('span');
+    author.appendChild(authorImg);
+    author.appendChild(authorName);
+    div.appendChild(headline);
+    div.appendChild(author);
+
+    return div;
+}
+
+const request = axios.get("https://lambda-times-backend.herokuapp.com/articles")
+    .then((axiosData) => {
+        console.log("data", axiosData);
+    })
+    
+    .catch((err) => {
+        console.log("There was an error", err);
+    });
